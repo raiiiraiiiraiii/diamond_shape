bb7de59ed99bbc768ad69866521bd049782c10f9
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-09-26 21:17:29 +0800

    Added Pseudocode

---
+++ diamond_shape.py
@@ -0,0 +1,5 @@
+# Write a Python function named print diamond that takes an odd integer n as an argument 
+# and prints a diamond shape with a width of n using the * character.
+
+# Note: If an even number is passed, the function should return "Please provide an odd integer."
+
